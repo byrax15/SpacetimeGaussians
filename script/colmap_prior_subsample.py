2797f8eccdbc952ff39ab37e635aa279ccc6a7ab
byrax15 <byraxis@gmail.com>
2025-03-31 20:21:03 -0400

    blender camera export downsampling and colmap point extraction with priors

---
+++ script/colmap_prior_subsample.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+from typing import Literal
+from pathlib import Path
+from tap import Tap
+from pathlib import Path
+import re
+import shutil
+
+NUMBER_CAMERAS = 1
+NUMBER_HEADER_LINES = 1
+NUMBER_LINES_PER_GROUP = 2
+
+
+def immediate_invoke(f): return f()
+
+
+OutModes = Literal["stdout", "newfile", "inplace"]
+
+
+def out_lines(mode: OutModes, lines: list[str], inpath: Path):
+    if mode == "stdout":
+        print("".join(lines))
+    elif mode == "newfile":
+        outpath = inpath.with_name(inpath.name + ".new")
+        with open(outpath, "w") as f:
+            f.writelines(lines)
+    elif mode == "inplace":
+        outpath = Path(inpath)
+        shutil.move(inpath, outpath.with_name(inpath.name + ".bak"))
+        with open(outpath, "w") as f:
+            f.writelines(lines)
+
+
+class ArgumentParser(Tap):
+    sparse_model_dir: Path
+    camera_subsample: int  # Preserve the first out of every N camera views
+    camera_image_pattern = re.compile(
+        r"([0-9]+) (Camera[.][0-9]{3})")
+    """A pattern matching and grouping the camera id and image name; must contain 2 groups"""
+    camera_new_name_format: str = "{cam_id} cam{image_number:03}.png"
+    camera_new_name_first_image = 1
+    out = OutModes
+
+    def configure(self):
+        self.add_argument(
+            "--out", choices=["stdout", "newfile", "inplace"], default="stdout")
+
+
+args = ArgumentParser().parse_args()
+
+
+@immediate_invoke
+def to_single_camera():
+    # convert to single camera
+    cameras_path = args.sparse_model_dir / "cameras.txt"
+    with open(cameras_path) as f:
+        lines = f.readlines()
+
+    filtered = lines[:1+NUMBER_HEADER_LINES]
+    out_lines(args.out, filtered, cameras_path)
+
+
+@immediate_invoke
+def downsample_images():
+    # downsample images file
+    def select_line(line_num: int):
+        if line_num < NUMBER_HEADER_LINES:
+            return True
+        return (line_num - NUMBER_HEADER_LINES) % (NUMBER_LINES_PER_GROUP * args.camera_subsample) < NUMBER_LINES_PER_GROUP
+
+    image_path = args.sparse_model_dir / "images.txt"
+    with open(image_path) as f:
+        lines = f.readlines()
+
+    filtered = [lines[i] for i in filter(select_line, range(len(lines)))]
+
+    # Renumber images and relink to single camera
+    image_number = args.camera_new_name_first_image
+    for i, line in enumerate(filtered):
+        match = args.camera_image_pattern.search(line)
+        if match and len(match.groups()) == 2:
+            filtered[i] = line.replace(
+                match.group(), args.camera_new_name_format.format(cam_id=0, image_number=image_number))
+            image_number += 1
+
+    out_lines(args.out, filtered, image_path)
--- script/pre_colmap_prior.py
+++ script/pre_colmap_prior.py
@@ -1,64 +1,120 @@
 
+from tap import Tap, tapify
+from pathlib import Path
+import re
+import os
 
-if __name__ == "__main__":
-    from tap import Tap, tapify
-    from pathlib import Path
-    import re
 
-    class ArgumentParser(Tap):
-        videosdir: Path
-        prior_data_fmt: str = "txt"
-        imageext: str = "png"
-        dryrun: bool = False
-        startframe: int = 0
-        endframe: int = 50
+class ArgumentParser(Tap):
+    videosdir: Path
+    prior_data_fmt: str = "txt"
+    imageext: str = "png"
+    no_single_camera: bool = False
+    camera_model: str = "SIMPLE_PINHOLE"
+    dryrun: bool = False
+    startframe: int = 0
+    endframe: int
 
-    class SparsePriorModel:
-        def __init__(self, videosdir: Path, prior_data_fmt: str):
-            self.prior = videosdir / "prior"
-            self.cameras = self.prior / f"cameras.{prior_data_fmt}"
-            self.images = self.prior / f"images.{prior_data_fmt}"
-            self.points3D = self.prior / f"points3D.{prior_data_fmt}"
 
-            assert self.prior.exists()
-            assert self.cameras.exists()
-            assert self.images.exists()
-            assert self.points3D.exists()
+class SparsePriorModel:
+    def __init__(self, videosdir: Path, prior_data_fmt: str):
+        self.prior = videosdir / "prior"
+        self.cameras = self.prior / f"cameras.{prior_data_fmt}"
+        self.images = self.prior / f"images.{prior_data_fmt}"
+        self.points3D = self.prior / f"points3D.{prior_data_fmt}"
 
-        def __repr__(self):
-            return f"""SparseModel(
-    prior = {self.prior},
-    cameras = {self.cameras},
-    images = {self.images},
-    points3D = {self.points3D}
+        assert self.prior.exists()
+        assert self.cameras.exists()
+        assert self.images.exists()
+        assert self.points3D.exists()
+
+    def __repr__(self):
+        return f"""SparseModel(
+prior = {self.prior},
+cameras = {self.cameras},
+images = {self.images},
+points3D = {self.points3D}
 )"""
 
-    def colmap_input_dir(point_dir: Path, frame: int) -> Path:
-        return point_dir / f"colmap_{frame}" / "input"
 
-    def colmap_input_image_path(point_dir: Path, frame: int, cam_num: int, imageext: str) -> Path:
-        return colmap_input_dir(point_dir, frame) / f"{cam_num}.{imageext}"
+def colmap_input_dir(point_dir: Path, frame: int) -> Path:
+    return point_dir / f"colmap_{frame}" / "input"
 
-    args = ArgumentParser().parse_args()
-    prior_data = tapify(SparsePriorModel, known_only=True)
-    frames_dir = args.videosdir / "frames"
-    assert frames_dir.exists()
-    point_dir = args.videosdir / "point"
-    assert point_dir.exists()
 
-    for f in range(args.startframe, args.endframe):
-        colmap_input_dir(point_dir, f).mkdir(parents=True, exist_ok=True)
+def colmap_input_image_path(point_dir: Path, frame: int, cam_num: int, imageext: str) -> Path:
+    return colmap_input_dir(point_dir, frame) / f"cam{cam_num:03}.{imageext}"
 
-    cam_pattern = re.compile(r"cam(\d+)")
-    frame_pattern = re.compile(r"(\d+)")
-    for cam in frames_dir.glob("cam*"):
-        cam_num = int(cam_pattern.match(cam.name)[1])
-        for frame in cam.glob(f"*.{args.imageext}"):
-            frame_num = int(frame_pattern.match(frame.name)[1])
-            link_path = colmap_input_image_path(
-                point_dir, frame_num, cam_num, args.imageext)
-            if args.dryrun:
-                print(f"'{link_path}'.symlink_to('{frame.absolute()}')")
-            else:
+
+args = ArgumentParser().parse_args()
+prior_data = SparsePriorModel(args.videosdir, args.prior_data_fmt)
+frames_dir = args.videosdir / "frames"
+assert frames_dir.exists()
+point_dir = args.videosdir / "point"
+
+for f in range(args.startframe, args.endframe):
+    colmap_input_dir(point_dir, f).mkdir(parents=True, exist_ok=True)
+
+cam_pattern = re.compile(r"cam(\d+)")
+frame_pattern = re.compile(r"(\d+)")
+for cam in frames_dir.glob("cam*"):
+    cam_num = int(cam_pattern.match(cam.name)[1])
+    for frame in cam.glob(f"*.{args.imageext}"):
+        frame_num = int(frame_pattern.match(frame.name)[1])
+        link_path = colmap_input_image_path(
+            point_dir, frame_num, cam_num, args.imageext)
+        if args.dryrun:
+            print(f"'{link_path}'.symlink_to('{frame.absolute()}')")
+        else:
+            try:
                 link_path.symlink_to(frame.absolute())
+            except FileExistsError:
+                pass
 
+
+class ColmapExecutor:
+    def __init__(self, point_dir: Path, frame_num: int, no_single_camera: bool, camera_model: str, dryrun: bool):
+        self.frame_dir = point_dir / f"colmap_{frame_num}"
+        self.database_path = self.frame_dir / "distorted" / "database.db"
+        self.input_path = self.frame_dir / "input"
+        self.sparse_path = self.frame_dir / "distorted" / "sparse"
+        self.dryrun = dryrun
+        self.commands = [
+            f"""colmap feature_extractor
+--database_path {self.database_path}
+--image_path {self.input_path}
+--ImageReader.single_camera {0 if no_single_camera else 1}
+--ImageReader.camera_model {camera_model}
+            """,
+            f"""colmap exhaustive_matcher
+--database_path {self.database_path}
+            """,
+            f"""colmap point_triangulator
+--database_path {self.database_path}
+--image_path {self.input_path}
+--output_path {self.sparse_path}
+--input_path {prior_data.prior}
+--Mapper.ba_global_function_tolerance=1e-6
+            """,
+            f"""colmap image_undistorter
+--image_path {self.input_path}
+--input_path {self.sparse_path}
+--output_path {self.frame_dir}
+--output_type COLMAP
+            """,
+        ]
+
+    def __call__(self):
+        for command in self.commands:
+            if (self.dryrun):
+                print(command)
+            else:
+                for dir in [self.sparse_path]:
+                    dir.mkdir(parents=True, exist_ok=True)
+                code = os.system(command.replace('\n', ' '))
+                assert code == 0
+
+
+for f in range(args.startframe, args.endframe):
+    executor = ColmapExecutor(
+        point_dir, f, args.no_single_camera, args.camera_model, args.dryrun)
+    executor()
